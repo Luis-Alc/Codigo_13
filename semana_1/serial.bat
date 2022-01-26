269bb4a5c912bc3c776950d4be42780e86550acd
Luis-Alc <alca120512@gmail.com>
2022-01-25 22:37:51 -0500

    Archivos Bat

---
+++ semana_1/serial.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Serial de Equipo
+echo ---------------------
+ipconfig
+
+echo ---------------------------------------------------------------------------
+
+
+
+echo Informacion de RED IP de equipo
+echo ---------------------
+ipconfig
+pause
+exit	
